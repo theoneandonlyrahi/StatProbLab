5e2f2f39d8492274fdf992f95a6f0659c5f40d84
Rahidul Alam <280557@stu.pctvs.org>
2026-05-23 18:18:27 -0400

    Added subpages for each card

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-function showPage(id) {
+function showPage(type, id) {
     // Hide all pages
     document.querySelectorAll('.page').forEach(page => {
         page.classList.remove('active');
     });
     
     // Show the selected page
-    document.getElementById("page-" + id).classList.add('active');
+    document.getElementById(type + "-" + id).classList.add('active');
 }
